81e36258588292191b380afd6e0c014c5df7bd26
felipe valencia <felipe.valencial@uqvirtual.edu.co>
2025-09-03 15:05:14 -0500

    login

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,15 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
+
 import { routes } from './app.routes';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { usuarioInterceptor } from './interceptor/usuario.interceptor';
+
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+ providers: [
+   provideRouter(routes),
+   provideHttpClient(withInterceptors([usuarioInterceptor]))
+ ]
 };
